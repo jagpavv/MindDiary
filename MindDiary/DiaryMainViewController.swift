2d5cee2cea3c90d5ff0bc56f7010f2eda8c5df18
Eunjin Kim <jagpavv@gmail.com>
2018-11-23 17:44:29 +0100

    Add textView placeHolder text

--- MindDiary/DiaryMainViewController.swift
+++ MindDiary/DiaryMainViewController.swift
@@ -5,83 +5,140 @@ let kNumberOfDiaryCard = 3
 class DiaryMainViewController: UIViewController, UITextViewDelegate {
 
   var diaryCardView: UIView!
+  var moodColorView: UIView!
+  var textView: UITextView!
+
   @IBOutlet weak var diaryCardBaseView: UIView!
 
 //  override func viewDidLoad() {
 //    super.viewDidLoad()
-//    showDiaryCard()
+//
 //  }
 
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     showDiaryCard()
-//    showDiaryCard2()
+
+    textViewDidBeginEditing(textView)
+    textViewDidEndEditing(textView)
   }
 
   func showDiaryCard() {
     let pad = CGFloat(10)
 
-    // DiaryCards
-    diaryCardView = UIView()
+    // 1 DiaryCards
     let totalHeight = diaryCardBaseView.frame.height
     let diaryCardWidth = diaryCardBaseView.frame.width
     let diaryCardHeight = (totalHeight - (pad * CGFloat(3 + 1))) / 3
 
-    // Inside of DiaryCards (mood color view) = mcv
-    let mcvWidth = diaryCardWidth / 4
-    let mcvTotalHeight = diaryCardHeight
-//    let mcvColor : [CGColor] = [#colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 1, green: 0.6235294118, blue: 0.03921568627, alpha: 1), #colorLiteral(red: 1, green: 0.2156862745, blue: 0.3725490196, alpha: 1)]
+    // 2 mood color view of DiaryCards
+    let moodColorViewWidth = diaryCardWidth / 4
+    let moodColorViewTotalHeight = diaryCardHeight
 
-    // text section
-    let textViewWidth = (diaryCardWidth - ((pad * 2) + mcvWidth))
+    // 3 text view of DiaryCards
+    let textViewWidth = (diaryCardWidth - ((pad * 2) + moodColorViewWidth))
     let textViewHeight = (diaryCardHeight - (pad * 2))
+    let xx = moodColorViewWidth + pad
+    let yy = pad
 
+    // draw views
     for idx in 0..<kNumberOfDiaryCard {
-      let x = CGFloat(0) //     * (diaryCardWidth + pad)
+      let x = CGFloat(0)
       let y = CGFloat(idx % 3) * (diaryCardHeight + pad)
 
-      // DiaryCards
-      let diaryCard = UIView(frame: CGRect(x: x, y: y, width: diaryCardWidth, height: diaryCardHeight))
-      diaryCard.layer.borderWidth = 1
-      diaryCard.layer.borderColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
-      self.diaryCardBaseView.addSubview(diaryCard)
+      // 1 DiaryCards
+      diaryCardView = UIView()
+      diaryCardView.frame = CGRect(x: x, y: y, width: diaryCardWidth, height: diaryCardHeight)
+      diaryCardView.layer.borderWidth = 1
+      diaryCardView.layer.borderColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+      diaryCardBaseView.addSubview(diaryCardView)
 
-      // Inside of DiaryCards (mood color view) = mcv
-      let mcv = UIView(frame: CGRect(x: 0, y: 0, width: mcvWidth, height: mcvTotalHeight))
-//      mcv.layer.backgroundColor = mcvColor[idx]
-      mcv.layer.backgroundColor = DiaryMainView.init().moodColor[idx]
-      diaryCard.addSubview(mcv)
+      // 2 mood color view of DiaryCards
+      moodColorView = UIView()
+      moodColorView.frame = CGRect(x: 0, y: 0, width: moodColorViewWidth, height: moodColorViewTotalHeight)
+      moodColorView.layer.backgroundColor = DiaryMainView.init().moodColor[idx]
+      diaryCardView.addSubview(moodColorView)
 
-      // text section
-      let xx = mcvWidth + pad
-      let yy = pad
+      // 3 text view of DiaryCards
+      textView = UITextView()
+      textView.frame = CGRect(x: xx, y: yy, width: textViewWidth, height: textViewHeight)
 
-      let textView = UITextView(frame: CGRect(x: xx, y: yy, width: textViewWidth, height: textViewHeight))
+      textView.delegate = self
+      textView.becomeFirstResponder()
+
+//      textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+
       textView.text = DiaryMainView.init().placeHolderTxt[idx]
       textView.textColor = UIColor.lightGray
-      textView.becomeFirstResponder()
-      textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
-
-      diaryCard.addSubview(textView)
+      diaryCardView.addSubview(textView)
     }
   }
 
-
-
-
-
-//  func showDiaryCard2() {
-//    diaryCardView = UIView()
-//    let totalWidth = diaryCardBaseView.frame.width
-//    let totalHeight = diaryCardBaseView.frame.height / 3
-//
-//    guard let diaryCardView = diaryCardView else { return }
-//
-//    diaryCardView.frame = CGRect(x: 0, y: 0, width: totalWidth, height: totalHeight)
-//    diaryCardView.backgroundColor = UIColor.gray
-////    if let diaryCardView = diaryCardView {
-//      self.diaryCardBaseView?.addSubview(diaryCardView)
-////  }
-//  }
+  func textViewDidBeginEditing(_ textView: UITextView) {
+    if textView.textColor == UIColor.lightGray {
+      textView.text = nil
+      textView.textColor = UIColor.black
+    }
   }
+
+  func textViewDidEndEditing(_ textView: UITextView) {
+    if textView.text.isEmpty {
+      textView.text = "Placeholder"
+      textView.textColor = UIColor.lightGray
+    }
+  }
+
+//  func placeCursorPosition(_ textView: UITextView) {
+//    let startPosition: UITextPosition = textView.beginningOfDocument
+//    let endPosition: UITextPosition = textView.endOfDocument
+//    let selectedRange: UITextRange? = textView.selectedTextRange
+//
+//    if let selectedRange = textView.selectedTextRange {
+//
+//      let cursorPosition = textView.offset(from: textView.beginningOfDocument, to: selectedRange.start)
+//
+//      print("\(cursorPosition)")
+//    }
+//  }
+
+//  func showPlaceHolderText(replacementText inputedText: String) {
+//    //    var currentText = textView.text
+//    let updateText = ""
+//
+//    if updateText.isEmpty {
+//      textView.text = "Zzzzzzzzzz"
+//      textView.textColor = UIColor.lightGray
+////      textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+//    }
+//
+//    if self.view.window != nil {
+//      if textView.textColor == UIColor.lightGray && !inputedText.isEmpty {
+//        textView.text.removeAll()
+//        textView.textColor = UIColor.black
+//        textView.text = inputedText
+//        textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+//      }
+//    }
+////    else if textView.textColor == UIColor.lightGray && !inputedText.isEmpty {
+////      textView.text.removeAll()
+////      textView.textColor = UIColor.black
+////      textView.text = inputedText
+////    }
+//  }
+//
+//  func textViewDidChangeSelection(_ textView: UITextView) {
+//    if self.view.window != nil {
+//      if textView.textColor == UIColor.lightGray {
+//        textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+//      }
+//    }
+//  }
+
+
+}
+
+
+extension UITextViewDelegate {
+
+}
 
